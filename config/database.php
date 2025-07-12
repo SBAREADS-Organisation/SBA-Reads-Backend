06cd0f1a778e2f6f3d3a2c83b430f46a7b822e32
Edward Ndiyo <52774709+Edwardndiyo@users.noreply.github.com>
2025-07-12 10:01:48 +0100

    Update database.php to fix db connection issues

--- config/database.php
+++ config/database.php
@@ -84,7 +84,8 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            // 'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'), // ✅ this is the fix
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
